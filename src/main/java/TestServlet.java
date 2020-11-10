dff39e690477f15a1d2b152b7bf5447c8c502f37
John McNay <john.w.mcnay@gmail.com>
2020-11-10 10:55:43 -0600

    fixed exception when trying to use JSTL

--- src/main/java/TestServlet.java
+++ src/main/java/TestServlet.java
@@ -5,11 +5,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "TestServlet", urlPatterns = "/")
 public class TestServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
+//        request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+
+//        HttpSession session = request.getSession();
+//        PrintWriter out = response.getWriter();
+//        response.setContentType("text/html");
+//
+//        Object id = session.getAttribute("id");
+//
+//        if (id == null) {
+//            request.setAttribute("message", "Welcome!");
+//            request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+//        } else {
+//            request.setAttribute("message", "Welcome back!");
+//        }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,16 +1,10 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: jwmcnay
-  Date: 11/9/20
-  Time: 2:55 PM
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
   <head>
     <title>$Title$</title>
   </head>
   <body>
-  $END$
+  $END$ test
   </body>
 </html>
